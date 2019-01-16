99408261077d39e73dbf30997230c7e30798ff99
lori <40824190+lalaoneo@users.noreply.github.com>
2019-01-16 10:35:57 +0800

    Create MultAnnotationClass.java

---
+++ src/main/java/com/lori/javademo/annotation/multAnnotation/MultAnnotationClass.java
@@ -0,0 +1,25 @@
+package com.lori.javademo.annotation.multAnnotation;
+
+@MultAnnotationDemo("multAnnotationDemo")
+public class MultAnnotationClass {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
