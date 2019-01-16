5e8ef7e596c1dd7d86d965f661521ee3a904fdde
lori <40824190+lalaoneo@users.noreply.github.com>
2019-01-16 11:43:20 +0800

    Create Person.java

---
+++ src/main/java/com/lori/javademo/innerClass/Person.java
@@ -0,0 +1,12 @@
+package com.lori.javademo.innerClass;
+
+public class Person {
+
+    public void eat(String string){
+        System.out.println("eating something "+ string);
+    }
+
+    public void hi(){
+        System.out.println("how are you");
+    }
+}
