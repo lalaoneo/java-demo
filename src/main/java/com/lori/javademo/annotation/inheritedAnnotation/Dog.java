54b94d4cdadac24665928fba2236a0ccb4d74174
lori <40824190+lalaoneo@users.noreply.github.com>
2019-01-16 10:35:09 +0800

    Create Dog.java

---
+++ src/main/java/com/lori/javademo/annotation/inheritedAnnotation/Dog.java
@@ -0,0 +1,14 @@
+package com.lori.javademo.annotation.inheritedAnnotation;
+
+public class Dog extends Animal {
+
+    private Integer sex;
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+}
