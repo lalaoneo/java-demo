e7b809d1fce4377ced72504b8b445e8438ce86fd
lori <40824190+lalaoneo@users.noreply.github.com>
2019-01-16 15:17:02 +0800

    Create Contains.java

---
+++ src/main/java/com/lori/javademo/common/Contains.java
@@ -0,0 +1,10 @@
+package com.lori.javademo.common;
+
+public class Contains {
+
+    public static final Integer countDownLatchNum = 7;
+
+    public static final Integer cyclicBarrierNum = 5;
+
+    public static final Integer ONE = 1;
+}
